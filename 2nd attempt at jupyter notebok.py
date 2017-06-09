5b40762f85d22de83213eb51994680b83e37f20f
Caitlin Keegan <ckeegan@mit.edu>
2017-06-08 23:26:19 -0400

    Create 2nd attempt at jupyter notebok.py

--- 2nd attempt at jupyter notebok.py
+++ 2nd attempt at jupyter notebok.py
@@ -10,14 +10,13 @@ import numpy as np
 
 # In[2]:xx
 
-    #this does the same thing as the below shell but with fewer lines 
 with open('dmv504n00.mmd.as.cn.co2.nl.mo.dat','r') as f:
     #next(f) # skip first row
     yeet = list(range(22))
     yeet[0] = 'DATE'
     yeet[4] = 'CO2'
     df = pd.DataFrame((l.rstrip().split() for l in f), columns=yeet)
-    #print(df)
+    
     copy_a_df = df.copy()
     copy_a_df = copy_a_df.drop(df.index[range(0,37)])    ##remove unnessary columns and rows
     copy_a_df = copy_a_df.drop(df.index[range(1,4)], axis=1)
@@ -25,11 +24,9 @@ with open('dmv504n00.mmd.as.cn.co2.nl.mo.dat','r') as f:
     copy_a_df = copy_a_df.drop(df.index[range(7,10)], axis=1)
     copy_a_df = copy_a_df.drop(df.index[10], axis=1)
     
-#print(copy_a_df.values)
 
 # In[ ]:
 
-    #here I will attempt to plot this data in a scatterplot
 copy_c_df = copy_a_df.copy()
 copy_c_df = copy_c_df.T
 copy_c_df = copy_c_df.drop(df.index[range(11,22)], axis=0)  #remove unnecessary column
@@ -45,8 +42,8 @@ copy_c_df = copy_c_df.drop(df.index[37])
 #copy_c_df = copy_c_df.T
 copy_d_df = copy_c_df.copy()
 
-copy_d_df['CO2'] = pd.to_numeric(copy_d_df['CO2'], errors = 'coerce')
-print(copy_d_df.index)
+copy_d_df['CO2'] = pd.to_numeric(copy_d_df['CO2'], errors = 'coerce')  #this converts from dtype:object to dtype:int64
+print(copy_d_df.index)  #confirms that the conversion was successful
                
 
 #copy_e_df = copy_e_df.copy()
@@ -55,6 +52,6 @@ copy_e_df = copy_d_df.copy()
 
 #print(copy_d_df.index)
 
-copy_e_df[['DATE', 'CO2']].set_index('DATE').plot(figsize=(15,8))  #uncommented when above is commented out  #doesn't reach this
+copy_e_df[['DATE', 'CO2']].set_index('DATE').plot(figsize=(15,8))  #uncommented when above is commented out  
 # In[]:
     
